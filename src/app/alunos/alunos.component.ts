acfd3d211a955d5d67d4115934cec015841e01d2
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-13 17:00:43 -0300

    Exibindo alunos na tela com ngFor

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alunos',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './alunos.component.html',
   styleUrl: './alunos.component.css'
 })
-export class AlunosComponent {
+export class AlunosComponent implements OnInit {
+  titulo = 'Alunos'
+  
+  public alunos = [
+    {nome: 'Maria', idade: 18}, 
+    {nome: 'João',  idade: 21},
+    {nome: 'Ana', idade: 28},
+    {nome: 'Pedro', idade: 15}, 
+    {nome: 'Carla', idade: 30}
+  ]
+  
+  constructor() {
 
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
