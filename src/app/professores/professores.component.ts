a00d115afd90f3c0be1787566f468545ebd69146
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-14 13:01:39 -0300

    Exibe a lista de professores utilizando NgFor

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-professores',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './professores.component.html',
   styleUrl: './professores.component.css'
 })
-export class ProfessoresComponent {
+export class ProfessoresComponent implements OnInit {
+  titulo = 'Professores'
 
+  professores = [
+    {nome: 'Carlos', idade: 38}, 
+    {nome: 'Maria',  idade: 41},
+    {nome: 'José', idade: 58},
+    {nome: 'Marcelo', idade: 55}, 
+    {nome: 'Vera', idade: 60}
+  ]
+
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
 }
