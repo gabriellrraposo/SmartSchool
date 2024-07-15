4a96688dba93d72ef8108f109d4638842a1be4d1
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-15 17:08:40 -0300

    Cria o componente titulo

---
+++ src/app/titulo/titulo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo',
+  standalone: true,
+  imports: [],
+  templateUrl: './titulo.component.html',
+  styleUrls: ['./titulo.component.css']
+})
+export class TituloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
