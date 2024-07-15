03b7644cfab3b6cb7c1cb2cf0a43451effc56923
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-15 16:31:15 -0300

    Cria componente perfil

---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
