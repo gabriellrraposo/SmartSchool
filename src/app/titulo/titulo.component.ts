e325787ee0e03e33cd8fab9ffb6bd766d7b912b4
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-15 18:07:42 -0300

    Adiciona um input à variável titulo

--- src/app/titulo/titulo.component.ts
+++ src/app/titulo/titulo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
@@ -9,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
 
-  constructor() { }
+  @Input() titulo: string;
+
+  constructor() {
+    this.titulo = ''
+  }
 
   ngOnInit() {
   }
