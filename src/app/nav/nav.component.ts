20e66457cda18b46d66cac0c635d45676d7dec5a
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-15 17:08:30 -0300

    Configura a barra de navegação

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
