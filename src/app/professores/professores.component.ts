3b9a02ddddc18cef7fff7a32f074aa90f1c36e7f
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-15 18:17:47 -0300

    Adiciona disciplinas aos professores

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -13,11 +13,11 @@ export class ProfessoresComponent implements OnInit {
   public titulo = 'Professores'
 
   professores = [
-    { id: 1, nome: "Fernanda", sobrenome: "Gomes", telefone: "(11) 92345-6789" },
-    { id: 2, nome: "Gustavo", sobrenome: "Lima", telefone: "(21) 93456-7890" },
-    { id: 3, nome: "Helena", sobrenome: "Almeida", telefone: "(31) 94567-8901" },
-    { id: 4, nome: "Igor", sobrenome: "Rodrigues", telefone: "(41) 95678-9012" },
-    { id: 5, nome: "Juliana", sobrenome: "Ferreira", telefone: "(51) 96789-0123" }
+    { id: 1, nome: "Fernanda", sobrenome: "Gomes", disciplina: "Matemática" },
+    { id: 2, nome: "Gustavo", sobrenome: "Lima", disciplina: "História" },
+    { id: 3, nome: "Helena", sobrenome: "Almeida", disciplina: "Geografia" },
+    { id: 4, nome: "Igor", sobrenome: "Rodrigues", disciplina: "Biologia" },
+    { id: 5, nome: "Juliana", sobrenome: "Ferreira", disciplina: "Química" }
   ]
 
   constructor() {}
