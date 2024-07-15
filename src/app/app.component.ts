9f38a8c005e371b91c4d9c9e5ce09ea6f663721c
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-15 16:31:48 -0300

    Configurando as rotas para os novos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AlunosComponent } from './alunos/alunos.component';
 import { ProfessoresComponent } from './professores/professores.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AlunosComponent, ProfessoresComponent, RouterLink],
+  imports: [RouterOutlet, AlunosComponent, ProfessoresComponent, PerfilComponent, DashboardComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProfessoresComponent } from './professores/professores.component';
 import { AlunosComponent } from './alunos/alunos.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: 'professores', component: ProfessoresComponent},
-    {path: 'alunos', component: AlunosComponent}
+    {path: 'alunos', component: AlunosComponent},
+    {path: 'perfil', component: PerfilComponent},
+    {path: 'dashboard', component: DashboardComponent}
 ];
