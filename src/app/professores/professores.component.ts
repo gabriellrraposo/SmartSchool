7c16bd60137edd55b1039a6dfa38a3a2f5c8d88d
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-16 21:31:02 -0300

    Faz form  aparecer a esquerda da tela quando seleciona um professor

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -1,12 +1,13 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TituloComponent } from '../titulo/titulo.component';
 import { Professor } from '../models/Professor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-professores',
   standalone: true,
-  imports: [NgFor, TituloComponent, NgIf],
+  imports: [NgFor, TituloComponent, NgIf, NgClass, FormsModule, ReactiveFormsModule],
   templateUrl: './professores.component.html',
   styleUrl: './professores.component.css'
 })
